c2d30ef9dc8b38e9a7085b432e3b454d5c6893ae
Dan Finlay <dan@danfinlay.com>
2018-05-18 13:31:53 -0700

    Add total

--- metamask.js
+++ metamask.js
@@ -6,5 +6,15 @@ async function run() {
 }
 
 run()
-.then(console.dir)
+.then((results) => {
+  console.dir(results)
+  return results
+})
+.then((results) => {
+  const total = results.reduce((total, item) => {
+    total += item.value
+    return total
+  }, 0)
+  console.dir(`Total: ${total}`)
+})
 .catch(console.error)
